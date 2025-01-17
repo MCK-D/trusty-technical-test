3d4a28f628548676f83cf6595707873928c90acc
mck@wsl <micka.daoud@epitech.eu>
2025-01-17 16:42:51 +0100

    [FEAT] exercise 2

---
+++ Exercice 2/server/app/controllers/jobs_controller.ts
@@ -0,0 +1,58 @@
+import env from '#start/env'
+import type { HttpContext } from '@adonisjs/core/http'
+import { Company, JobDetails } from '../interfaces/Company.js';
+
+function formatAddress(city: string, state: string, country: string): string {
+  const parts = [city, state, country].filter(part => part !== "");
+  return parts.join(", ");
+}
+
+
+export default class JobsController {
+  async getOpenJobs({ params, request, response }: HttpContext) {
+    const company = params.companyName;
+    const { department, contract, location } = request.qs();
+
+    if (!company)
+      return response.status(400).json({error: 'A company name is required'});
+
+    const api_url = `${env.get('WORKABLE_API_URL')}/api/v1/widget/accounts/${company}?details=true`;
+
+    try {
+      const res = await fetch(api_url);
+      const { name, jobs } = await res.json() as Company;
+
+      const filteredJobs = jobs.filter((job) => {
+        return (
+          (!department || job.department === department) &&
+          (!contract || job.employment_type === contract) &&
+          (!location || formatAddress(job.city, job.state, job.country) === location)
+        );
+      });
+
+      const formatJob = (job: JobDetails) => ({
+        ats_id: job.shortcode,
+        name: job.title,
+        jobUrl: job.url,
+        department: job.department,
+        contract: job.employment_type,
+        education: job.education ?? '',
+        experience: job.experience,
+        position: job.function,
+        location: formatAddress(job.city, job.state, job.country),
+        publishedAt: job.published_on,
+      });
+
+
+      return response.json({
+        companyName: name,
+        endpointsats: api_url,
+        nbrOpenJobs: filteredJobs.length,
+        jobs: filteredJobs.map(formatJob),
+      })
+
+    } catch (error) {
+      return response.status(500).json({error: `Could not fetch data for ${company}.`});
+    }
+  }
+}
